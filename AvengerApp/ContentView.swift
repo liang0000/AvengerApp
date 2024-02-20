e7db94c045170585f9537f6986c68644b262aa9a
Liang0 <saurabh.karajgikar@dhl.com>
2024-02-20 21:11:07 +0800

    Initial Commit

---
+++ AvengerApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

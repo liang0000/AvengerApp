63488a6cff51be4d3c6d692be03919c7ae17d0a2
Liang0 <saurabh.karajgikar@dhl.com>
2024-02-25 16:51:54 +0800

    update

--- AvengerApp/Models/Avenger.swift
+++ AvengerApp/Models/Avenger.swift
@@ -11,12 +11,6 @@ class AllAvenger {
         Avenger(name: "Iron Man", imgName: "ironman", rating: 2),
         Avenger(name: "Superman", imgName: "superman", rating: 1),
     ]
-    
-    func updateRating(for avenger: Avenger, rating: Int) {
-        if let index = avengers.firstIndex(where: { $0.id == avenger.id }) {
-            avengers[index].rating = rating
-        }
-    }
 }
 
 struct Avenger: Identifiable {
